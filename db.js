639f2abce0a732ac722b70be0e96da07c82dd630
Anubhav <121851863+Anubhavprogramer@users.noreply.github.com>
2023-12-28 18:44:18 +0530

    updates

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true";
+const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true";   //database string 
 
 async function connectToMongo() {
   try {
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const connecttomongo=require('./db')
+const connecttomongo=require('./db');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{          // home route
+    res.send('radhe radhe')     //sending response to the server
+})
+
+app.listen(port,()=>{               //listening to the port that is 3000
+    console.log("Now we are online");
+})
+
 
 connecttomongo()
 
